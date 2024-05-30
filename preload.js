2dcd0237b0a45f2d462178f04d31f6091f46cd89
rrefiew <105153938+rrefiew@users.noreply.github.com>
2024-05-30 22:58:11 +0500

    чета добавила

--- preload.js
+++ preload.js
@@ -2,9 +2,6 @@ const { contextBridge, ipcRenderer } = require("electron");
 //const crypto = require("node:crypto");
 
 contextBridge.exposeInMainWorld("versions", {
-  node: () => process.versions.node,
-  chrome: () => process.versions.chrome,
-  electron: () => process.versions.electron,
   ping: () => ipcRenderer.invoke("ping"),
 });
 // TODO: Fix this
